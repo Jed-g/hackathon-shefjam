53c0537721e102f77f60a5371bb8cb3c9e2c1a4e
Jed G <jed.golebiewski@gmail.com>
2022-02-13 13:21:52 +0000

    Build

---
+++ precache-manifest.75ab02b7de9a825e83168549935c51bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff935e5b2ecb16440a86638a1f17cc35",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fefecac694710114d1df",
+    "url": "/static/css/main.48194697.chunk.css"
+  },
+  {
+    "revision": "dd7a2e25d3f4c8c3988d",
+    "url": "/static/js/2.309f5280.chunk.js"
+  },
+  {
+    "revision": "bd96a1cfc47c001e68467155960da745",
+    "url": "/static/js/2.309f5280.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "017259590c4dfdae6d7f",
+    "url": "/static/js/3.8f4dbbe0.chunk.js"
+  },
+  {
+    "revision": "fefecac694710114d1df",
+    "url": "/static/js/main.0c59d4a3.chunk.js"
+  },
+  {
+    "revision": "a852180fbfe78a2c623b",
+    "url": "/static/js/runtime-main.7dec6997.js"
+  }
+]);
